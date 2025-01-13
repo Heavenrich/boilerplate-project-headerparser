f1cc9b994ecd01259d745d2b3e7c93bc5ad9b9bc
Heavenrich <sheavenrich@gmail.com>
2025-01-13 07:33:15 -0500

    json key change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 app.get('/api/whoami', function (req, res) {
 
   res.json({ 
-    ip: req.ip,  
+    ipaddress: req.ip,  
     language: req.headers['user-agent'],
     software: req.headers['accept-language']
   });
