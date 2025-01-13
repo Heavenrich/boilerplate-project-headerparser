e91c6a2fb284a9bd5240787ddfe81c4b6eb687db
Heavenrich <sheavenrich@gmail.com>
2025-01-13 07:32:14 -0500

    Complete Project

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+
+  res.json({ 
+    ip: req.ip,  
+    language: req.headers['user-agent'],
+    software: req.headers['accept-language']
+  });
 });
 
 // listen for requests :)
